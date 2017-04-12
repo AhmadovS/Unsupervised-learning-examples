fe01c1dea3d31191fcd3be0bb9e618b90473cccb
Navie <naviechan@gmail.com>
2017-04-12 18:12:23 -0400

    ha

--- iwae/datasets.py
+++ iwae/datasets.py
@@ -119,6 +119,7 @@ def binarized_face(n_validation=400):
     test_data = np.load(os.path.join(config.DATASETS_DIR, 'FACE', 'toronto_face_test.npy'))
 
     img = train_data[0]
+    img = img.reshape((1, 784))
     import matplotlib
     matplotlib.use('Agg')
     import matplotlib.pyplot as plt
@@ -126,7 +127,7 @@ def binarized_face(n_validation=400):
     plt.savefig(os.path.join(directory_name, "ha.png"))
     plt.close()
     sys.exit(0)
-    
+
     return BinarizedDatasetTheano(DatasetTheano(train_data, test_data, n_validation, shuffle=False))
     # return DatasetTheano(train_data, test_data, n_validation, shuffle=False)
 
