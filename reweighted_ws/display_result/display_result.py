da4cee439fc45db385d6e579f7ce67357472ad0e
Navie <naviechan@gmail.com>
2017-04-13 16:19:15 -0400

    ha

--- reweighted_ws/display_result/display_result.py
+++ reweighted_ws/display_result/display_result.py
@@ -12,7 +12,7 @@ def display_fields(F, n_fields=24, D=28):
 
 result_location = "../mnits/output2/bs100-lr13-si2-spl10-sbn-sbn-200-200-10.2017-04-13-18-53/result.h5"
 hparams = {}
-with tables.openFile(result_location, "r") as h5:
+with tables.open_file(result_location, "r") as h5:
     Lp_10 = h5.root.Lp_10[:]
     Lp_25 = h5.root.Lp_25[:]
     Lp_100 = h5.root.Lp_100[:]
